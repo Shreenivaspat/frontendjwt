e6a61bb47897ee4872f406d18fcdb6916c6536b1
Shreenivaspat <130136528+Shreenivaspat@users.noreply.github.com>
2023-12-10 20:21:50 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -21,7 +21,7 @@ function Register() {
     event.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/register",
+        "https://backendjwt.vercel.app/register",
         {
           ...values,
         },
